e3ea4da0e6f3720841c83c8f9550c071d13470ff
Theodore Turocy <ted.turocy@gmail.com>
2026-06-23 10:33:40 +0100

    Implement share rule contest and determine earnings

--- contest/__init__.py
+++ contest/__init__.py
@@ -32,11 +32,25 @@ class Group(BaseGroup):
         for player in self.get_players():
             player.setup_round()
 
+    def determine_outcome(self):
+        total = sum(player.tickets_purchased for player in self.get_players())
+        for player in self.get_players():
+            try:
+                player.prize_won = player.tickets_purchased / total
+            except ZeroDivisionError:
+                player.prize_won = 1 / len(self.get_players())
+            player.earnings = (
+                player.endowment -
+                player.tickets_purchased * player.cost_per_ticket +
+                self.prize * player.prize_won
+            )
 
 class Player(BasePlayer):
     endowment = models.CurrencyField()
     cost_per_ticket = models.CurrencyField()
     tickets_purchased = models.IntegerField()
+    prize_won = models.FloatField()
+    earnings = models.CurrencyField()
 
     def setup_round(self):
         self.endowment = C.ENDOWMENT
@@ -66,7 +80,12 @@ class Decision(Page):
 
 
 class WaitForDecisions(WaitPage):
-    pass
+    wait_for_all_groups = True
+
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        for group in subsession.get_groups():
+            group.determine_outcome()
 
 
 class Outcome(Page):
